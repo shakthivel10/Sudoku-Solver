0a120f076d7e834724609176460f6e28fc5c59cf
shakthivel <shakthivels2803@gmail.com>
2020-11-22 18:46:50 -0500

    Python Solver isSolved method

--- solver.py
+++ solver.py
@@ -29,3 +29,39 @@ class SudokuSolver:
                 return
 
         board[i][j] = "."
+
+    def isSolved(self, board):
+
+        for row in board:
+            present = [False for k in range(len(board))]
+            for n in row:
+                if n != ".":
+                    present[int(n)-1] = True
+                else:
+                    return False
+            if not all(present):
+                return False
+
+        for i in range(len(board)):
+            present = [False for k in range(len(board))]
+            for j in range(len(board)):
+                if board[j][i] != ".":
+                    present[int(board[j][i])-1] = True
+                else:
+                    return False
+            if not all(present):
+                return False
+
+        for i in range(int(pow(len(board), 0.5))):
+            for j in range(int(pow(len(board), 0.5))):
+                present = [False for k in range(len(board))]
+                for i1 in range(int(pow(len(board), 0.5))):
+                    for j1 in range(int(pow(len(board), 0.5))):
+                        if board[3*i+i1][3*j+j1] != ".":
+                            present[int(board[3*i+i1][3*j+j1]) - 1] = True
+                        else:
+                            return False
+                if not all(present):
+                    return False
+
+        return True
