5ec616c2e39423acdf93c9554651697d7359aac7
shakthivel <shakthivels2803@gmail.com>
2020-11-22 19:12:22 -0500

    Complete Python Solver

--- solver.py
+++ solver.py
@@ -65,3 +65,22 @@ class SudokuSolver:
                     return False
 
         return True
+
+    def getPossibleValues(self, board, i, j):
+
+        possibleValues = set([str(k) for k in range(1, len(board)+1)])
+
+        s = set(board[i])
+        s = s.union(set([board[k][j] for k in range(len(board))]))
+        s2 = set()
+
+        iStart = (i//3) * 3
+        jStart = (j//3) * 3
+
+        for i1 in range(iStart, iStart+3):
+            for j1 in range(jStart, jStart+3):
+                s2.add(board[i1][j1])
+
+        s = s.union(s2)
+
+        return possibleValues.difference(s)
